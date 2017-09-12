998421483f56cbc0e189e83bb00a4fc93986a2c7
alexandraalday <alexandra.alday@gmail.com>
2017-09-11 22:41:02 -0700

    user can save song to their saved songs list

---
+++ db/migrate/20170912031248_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :songs do |t|
+      t.string :artist
+      t.string :title
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
