6db0cbd409ad768b024aea8cd9d0168533af5fc7
alexandraalday <alexandra.alday@gmail.com>
2017-09-13 12:00:55 -0700

    cors no array

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:2000', 'https://modus-app.herokuapp.com']
+    origins 'localhost:2000', 'https://modus-app.herokuapp.com'
 
     resource '*',
       headers: :any,
